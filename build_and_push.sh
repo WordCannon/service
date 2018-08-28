b6651a5d1f451a5699ac162e4caca6177d103822
Maciej Adwent <maciekadwent@gmail.com>
2018-08-28 16:54:57 -0700

    impl

---
+++ build_and_push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t maciekdna/wordcannon-service .
+docker push maciekdna/wordcannon-service
---
+++ service.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const words = Object.keys(require("./words.json"));
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const getWord = () => {
+  const randIndex = Math.floor(words.length * Math.random());
+  return words[randIndex];
+}
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send(`${getWord()}\n`);
+});
+
+app.listen(PORT, HOST);
+console.log(`Firing cannon on http://${HOST}:${PORT}`);
