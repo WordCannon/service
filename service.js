796b0d555cbe6134dfd0aa3cdb0ef92a258e0898
Dave Cadwallader <dcadwallader@gmail.com>
2018-09-25 20:45:51 -0700

    adopt docker best practices

--- service.js
+++ service.js
@@ -15,5 +15,34 @@ app.get('/word', (req, res) => {
   res.send(`${getWord()}\n`);
 });
 
-app.listen(PORT, HOST);
+app.get('/healthz', function (req, res) {
+  res.send('I am happy and healthy\n');
+});
+
+const server = app.listen(PORT, HOST);
+
+// quit on ctrl-c when running docker in terminal
+process.on('SIGINT', function onSigint () {
+	console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+  shutdown();
+});
+
+// quit properly on docker stop
+process.on('SIGTERM', function onSigterm () {
+  console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+  shutdown();
+});
+
+// shut down server
+function shutdown() {
+  server.close(function onServerClosed (err) {
+    if (err) {
+      console.error(err);
+      process.exitCode = 1;
+    }
+		process.exit();
+  })
+}
+
 console.log(`Firing cannon on http://${HOST}:${PORT}`);
+
